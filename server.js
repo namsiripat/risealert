8084cee05a777475d12e5e9b3a797952dcac09bb
Sirapat Siribandit <bonkungga@hotmail.com>
2018-05-03 21:32:44 +0700

    fix http request in register.

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const path			= require('path')
 const os			= require('os')
 const mongoose		= require('mongoose')
 
-const userModel = require('./models/user');
+// eslint-disable-next-line
+const userModel     = require('./models/user')
 const User			= mongoose.model('Users')
 
 // connect to database
@@ -21,22 +22,15 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '/public')))
 
 let interfaces = os.networkInterfaces()
-var addresses = []
+let addresses = []
 for (var k in interfaces) {
-	for (var k2 in interfaces[k]) {
-		var address = interfaces[k][k2]
-		if (address.family === 'IPv4' && !address.internal) {
-			addresses.push(address.address)
-		}
-	}
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2]
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address)
+        }
+    }
 }
-
-var accelData
-var Room1
-
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html')
-})
 
 app.get('/patient', function (req, res) {
     var query = { sort: { name: 1 } }
@@ -48,37 +42,40 @@ app.get('/patient', function (req, res) {
 })
 
 app.post('/register', function (req, res) {
-	var newUser = new User(req.body)
-	newUser.save(function(err, user){
-		if(err) throw err
-		res.json(user)
-	})
+    let newUser = new User(req.body)
+    newUser.save(function(err, user){
+        if(err) throw err
+        res.status(200).json(user)
+    })
 })
 
 app.post('/accel/:id', function (req, res) {
-	console.log(req.params.id)
-	console.log(req.body)
-	var object = {
-		id: req.params.id,
-		status: 'error'
-	}
-	io.emit('accelData', object)
-	setTimeout(function () {
-		io.emit('accelData', {
-			id: req.params.id,
-			status: 'normal'
-		})
-	}, 11000)
+    let object = {
+        id: req.params.id,
+        status: 'error'
+    }
+    io.emit('accelData', object)
+    setTimeout(function () {
+        io.emit('accelData', {
+            id: req.params.id,
+            status: 'normal'
+        })
+    }, 11000)
+    res.sendStatus(200)
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', function (socket) {
-	socket.on('accelData', function (data) {
-		console.log(data)
-	})
+    socket.on('accelData', function (data) {
+        console.log(data)
+    })
 })
 
 server.listen(3000, process.argv[2], function () {
-	var host = server.address().address
-	var port = server.address().port
-	console.log('Server listening on %s:%s...', host, port)
+    let host = server.address().address
+    let port = server.address().port
+    console.log('Server listening on %s:%s...', host, port)
 })
